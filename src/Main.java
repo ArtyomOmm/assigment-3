da78f592092e936aec0979b46b1932d75342a081
ommar <maksim.logika@gmail.com>
2026-05-02 21:15:28 +0500

    final test main pass

--- src/Main.java
+++ src/Main.java
@@ -2,19 +2,58 @@ import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-        // Создаем таблицу с бОльшим количеством бакетов для 10000 элементов
-        MyHashTable<MyTestingClass, String> table = new MyHashTable<>(100);
+        System.out.println("=== ТЕСТ 1: MyHashTable ===");
+
+
+        MyHashTable<MyTestingClass, String> table = new MyHashTable<>(10);
         Random random = new Random();
 
-        // Добавляем 10000 случайных элементов
+
         for (int i = 0; i < 10000; i++) {
             int randomId = random.nextInt(100000);
-            String randomName = "Student" + random.nextInt(1000);
-            MyTestingClass key = new MyTestingClass(randomId, randomName);
-            table.put(key, "Value " + i);
+            String name = "Name" + i;
+            table.put(new MyTestingClass(randomId, name), "Value" + i);
         }
 
-        // Проверяем распределение
+
+        System.out.println("Распределение элементов по бакетам:");
         table.printBucketSizes();
+
+
+        MyTestingClass testKey = new MyTestingClass(999, "Special");
+        table.put(testKey, "Found It!");
+        System.out.println("\nПоиск ключа (999, Special): " + table.get(testKey));
+        table.remove(testKey);
+        System.out.println("После удаления: " + table.get(testKey));
+
+        System.out.println("\n=== ТЕСТ 2: BST (Binary Search Tree) ===");
+
+        BST<Integer, String> tree = new BST<>();
+
+
+        tree.put(50, "Root");
+        tree.put(30, "Left");
+        tree.put(70, "Right");
+        tree.put(20, "Left-Left");
+
+
+
+        System.out.println("Размер дерева (size): " + tree.size());
+
+
+        System.out.println("Обход дерева (In-order traversal):");
+        for (var elem : tree) {
+            System.out.println("key is " + elem.getKey() + " and value is " + elem.getValue());
+        }
+
+
+        System.out.println("\nУдаляем узел с ключом 30 (у которого два ребенка)");
+        tree.delete(30);
+        System.out.println("Новый размер дерева: " + tree.size());
+
+        System.out.println("Обход дерева после удаления:");
+        for (var elem : tree) {
+            System.out.print(elem.getKey() + " ");
+        }
     }
 }
